e3389fd07034a2512a5377f662bd801517eeb5bf
Victor <victorrafaelravazi1234@gmail.com>
2025-05-27 21:48:24 -0300

    sistema melhorado e muito mais interessante

--- main.py
+++ main.py
@@ -11,15 +11,63 @@ def home():
 @app.route("/api/diagnostico", methods=["POST"])
 def diagnostico():
     data = request.json
-    MedoDeSairDeCasa = data.get("MedoDeSairDeCasa")
-    data = request.json
-    Cascudo = data.get("Cascudo")
-    data = request.json
-    AmorReciproco = data.get("AmorReciproco")
-    if MedoDeSairDeCasa == 's' and Cascudo == 's' and AmorReciproco == 's':
-        return jsonify({"resposta": "Você esta virando um tatu"})
+
+    sexo = data.get("sexo")
+    altura = float(data.get("altura"))
+    estrutura = data.get("estrutura")
+    musculo = data.get("musculo")
+    definicao = data.get("definicao")
+
+    pontos = 0
+
+    if sexo == "masculino":
+        if altura <= 1.69:
+            pontos += 7
+        elif 1.70 <= altura <= 1.78:
+            pontos += 10
+        elif 1.79 <= altura <= 1.85:
+            pontos += 5
+        else:
+            pontos += 0
+    elif sexo == "feminino":
+        if altura <= 1.56:
+            pontos += 7
+        elif 1.57 <= altura <= 1.65:
+            pontos += 10
+        elif 1.66 <= altura <= 1.75:
+            pontos += 5
+        else:
+            pontos += 0
+    if estrutura == "ombros_largos":
+        pontos += 10
+    elif estrutura == "iguais":
+        pontos += 5
+    if musculo == "muito_facil":
+        pontos += 20
+    elif musculo == "facil":
+        pontos += 15
+    elif musculo == "dificil":
+        pontos += 5
+    elif musculo == "muito_dificil":
+        pontos -= 15
+    if definicao == "facil":
+        pontos += 10
+    elif definicao == "normal":
+        pontos += 5
+    elif definicao == "sofrido":
+        pontos += 0
+    if pontos >= 47:
+        resultado = "Genética perfeita"
+    elif 37 <= pontos <= 46:
+        resultado = "Genética muito boa"
+    elif 30 <= pontos <= 36:
+        resultado = "Genética boa"
+    elif 15 <= pontos <= 29:
+        resultado = "Genética mediana"
     else:
-        return jsonify({"resposta": "Pereça"})
+        resultado = "Genética ruim"
+
+    return jsonify({"resposta": f"Sua pontuação foi {pontos}. {resultado}."})
 
 if __name__ == "__main__":
     app.run(debug=True)
